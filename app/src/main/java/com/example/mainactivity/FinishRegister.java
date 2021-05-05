0eb2a0747eb2e854c6670323b756d9680081d2b5
Kane-TM <74352301+Kane-TM@users.noreply.github.com>
2021-05-05 14:04:05 +0700

    origin-commit

---
+++ app/src/main/java/com/example/mainactivity/FinishRegister.java
@@ -0,0 +1,14 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinishRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish_register);
+    }
+}
---
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_login);
+//        setContentView(R.layout.activity_register);
+//        setContentView(R.layout.activity_user_info);
+//        setContentView(R.layout.activity_main_tool_bar);
+//        setContentView(R.layout.activity_update_user_info);
+//        setContentView(R.layout.activity_finish_register);
+        setContentView(R.layout.activity_update_user_password);
+    }
+}
