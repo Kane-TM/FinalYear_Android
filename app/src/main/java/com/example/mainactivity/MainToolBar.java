d7f26fc91d643f2e6eff51a1e9bf42bd3a386b4a
Kane-TM <74352301+Kane-TM@users.noreply.github.com>
2021-05-06 15:47:10 +0700

    add icon logo in top nav bar & finish menu

--- app/src/main/java/com/example/mainactivity/MainToolBar.java
+++ app/src/main/java/com/example/mainactivity/MainToolBar.java
@@ -2,6 +2,7 @@ package com.example.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainToolBar extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainToolBar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tool_bar);
+    }
+
+    public void LaunchMenu(View view) {
+
     }
 }
